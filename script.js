6f9a5128783b83f4a6fee3211ae09dd3626722c3
MJspitta <madu.jang93@gmail.com>
2022-06-21 14:11:11 +0100

    First deployment

---
+++ script.js
@@ -0,0 +1,7 @@
+const studFullName = "name: MADU JANG"
+let studHeight = "height: 1.92 meters"
+const studCountry = "country: NIGERIA"
+
+document.getElementById('name').innerHTML = studFullName
+document.getElementById('height').innerHTML = studHeight
+document.getElementById('country').innerHTML = studCountry
